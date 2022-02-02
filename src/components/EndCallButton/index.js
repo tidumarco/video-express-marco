acc6285e8fca3f701c710526e26b7185f275a80c
Marco Tidu <matidu@utu.fi>
2022-02-02 09:20:51 +0200

    Changed endcall button

--- src/components/EndCallButton/index.js
+++ src/components/EndCallButton/index.js
@@ -4,17 +4,22 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 export default function EndCallIcon({ classes, handleEndCall }) {
   return (
-    <Tooltip title="End call" aria-label="add">
-      <IconButton
-        edge="start"
-        color="inherit"
-        aria-label="videoCamera"
-        className={classes.toolbarButtons}
-        style={{ backgroundColor: '#D50F2C' }}
-        onClick={handleEndCall}
-      >
-        <ExitToAppIcon fontSize="inherit" />
-      </IconButton>
-    </Tooltip>
+    // <Tooltip title="End call" aria-label="add">
+    //   <IconButton
+    //     edge="start"
+    //     color="inherit"
+    //     aria-label="videoCamera"
+    //     className={classes.toolbarButtons}
+    //     style={{ backgroundColor: '#D50F2C' }}
+    //     onClick={handleEndCall}
+    //   >
+    //     <ExitToAppIcon fontSize="inherit" />
+    //   </IconButton>
+    // </Tooltip>
+    <ThemeSelector>
+  <Button color="primary">
+    Button
+  </Button>
+</ThemeSelector>
   );
 }
